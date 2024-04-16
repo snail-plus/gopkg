78db7a57713322146b57aeb918000c00f16bf58f
冷飒 <308306362@qq.com>
2024-04-16 14:15:12 +0800

    init

---
+++ config/v1beta1/doc.go
@@ -0,0 +1,10 @@
+// Copyright 2022 Lingfei Kong <colin404@foxmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://github.com/superproj/onex.
+//
+
+// +k8s:deepcopy-gen=package
+// +k8s:conversion-gen=github.com/snail-plus/gopkg/config
+
+package v1beta1 // import "github.com/snail-plus/gopkg/config/v1beta1"
