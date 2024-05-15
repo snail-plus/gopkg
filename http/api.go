5975ae52339671f703e00e6113e76dbc6963e459
冷飒 <308306362@qq.com>
2024-05-15 16:42:37 +0800

    初始化仓库代码

---
+++ http/api.go
@@ -0,0 +1,42 @@
+package http
+
+import (
+	"gitee.com/eve_3/gopkg/log"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type Response struct {
+	Code SystemErrCode `json:"code"`
+	Msg  string        `json:"msg"`
+	Data interface{}   `json:"data"`
+}
+
+type Api struct{}
+
+func (r Api) Success(c *gin.Context, data any) {
+	c.JSON(http.StatusOK, Response{Data: data, Code: SystemErrCodeSuccess})
+}
+
+func (r Api) Failure(c *gin.Context, err error) {
+	log.Infof("Failure: %v", err)
+	c.JSON(http.StatusOK, Response{
+		Code: SystemErrCodeFailure,
+		Msg:  err.Error(),
+	})
+}
+
+func (r Api) Result(c *gin.Context, data any, err error) {
+	if err != nil {
+		r.Failure(c, err)
+	} else {
+		r.Success(c, data)
+	}
+}
+
+type SystemErrCode int
+
+const (
+	SystemErrCodeSuccess SystemErrCode = 0
+	SystemErrCodeFailure SystemErrCode = 90000
+)
