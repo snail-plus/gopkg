89b6ea00c28a37bbf44f794bc7bac6a51ed4b82f
冷飒 <308306362@qq.com>
2024-05-17 08:00:08 +0800

    初始化仓库代码

--- config/v1beta1/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2024 eve.  All rights reserved.
-
-// +k8s:deepcopy-gen=package
-// +k8s:conversion-gen=gitee.com/eve_3/gopkg/config
-
-package v1beta1 // import "gitee.com/eve_3/gopkg/config/v1beta1"
